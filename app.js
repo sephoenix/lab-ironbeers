0257bc472bb9a4b4503f88150e05085bab07c131
sephoenix <se.phoenix@gmail.com>
2021-11-24 21:32:21 +0100

    pre

--- app.js
+++ app.js
@@ -22,4 +22,25 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  res.render('/beers');
+});
+
+app.get('/random-beer', (req, res) => {
+  res.render('/random-beer');
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
+punkAPI
+  .getBeers()
+  .then(beersFromApi => console.log('Beers from the database: ', beersFromApi))
+  .catch(error => console.log(error));
+
+
+punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    // your magic happens here
+  })
+  .catch(error => console.log(error));
